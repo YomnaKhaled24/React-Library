13fcf5e02ee5a43dc2e8b150370668ffc6b6c557
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-05-26 19:32:56 +0300

    Update services.js

--- src/Pages/services.js
+++ src/Pages/services.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../Components/Header.js";
+import Header from "../Components/header.js";
 import ServicesComponent from "../Components/services.js";
 import { useTranslation } from "react-i18next";
 
